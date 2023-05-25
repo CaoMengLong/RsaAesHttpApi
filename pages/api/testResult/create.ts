84cdd29959598dd65789e0a2d8ad54712f2791f1
Menglong Cao <japan.menglong@gmail.com>
2023-05-25 14:43:22 +0900

    功能实现

--- pages/api/testResult/create.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "../../../lib/prismadb";
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-import { getServerSession } from 'next-auth';
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { testId } = req.body;
-  res.json("OK");
-}
